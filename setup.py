953a925d15a17be64b82aa033b42b91213fe3b4b
F0x <mail@f0x.me>
2014-11-03 21:39:07 +0100

    Added pycheckjpeg script

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from distutils.core import setup, Extension
 
-setup(name="pycheckjpeg", version="0.1",
-	ext_modules = [Extension("pycheckjpeg", ["pycheckjpeg.c"], libraries=['jpeg'])])
+setup(
+    name="pycheckjpeg", 
+    version="0.1",
+    ext_modules = [Extension("pycheckjpeg", ["pycheckjpeg.c"], libraries=['jpeg'])],
+    scripts=['scripts/pycheckjpeg']
+    )
