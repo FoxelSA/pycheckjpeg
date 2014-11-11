07dda1682541dcde285ad749bb333e9e0b7c930d
Kevin Velickovic <kevin.velickovic@gmail.com>
2014-11-11 11:53:45 +0100

    Rearranged folders

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup, Extension
 setup(
     name="pycheckjpeg", 
     version="0.1",
-    ext_modules = [Extension("pycheckjpeg", ["pycheckjpeg.c"], libraries=['jpeg'])],
+    ext_modules = [Extension("pycheckjpeg", ["src/pycheckjpeg.c"], libraries=['jpeg'])],
     scripts=['scripts/pycheckjpeg']
     )
