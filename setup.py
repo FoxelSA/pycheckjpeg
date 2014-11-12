d4625991d67470f20514f9298b66d8944e919aef
Kevin Velickovic <kevin.velickovic@gmail.com>
2014-11-12 11:49:15 +0100

    Formated setup.py

--- setup.py
+++ setup.py
@@ -42,8 +42,20 @@
 from distutils.core import setup, Extension
 
 setup(
-    name="pycheckjpeg", 
-    version="0.1",
-    ext_modules = [Extension("pycheckjpeg", ["src/pycheckjpeg.c"], libraries=['jpeg'])],
-    scripts=['scripts/pycheckjpeg']
-    )
+        name="pycheckjpeg",
+        version="0.1",
+        ext_modules = [
+            Extension(
+                "pycheckjpeg",
+                [
+                    "src/pycheckjpeg.c"
+                ],
+                libraries=[
+                   'jpeg'
+                ]
+            )
+        ],
+        scripts=[
+            'scripts/pycheckjpeg'
+        ]
+)
