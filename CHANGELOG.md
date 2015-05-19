83b4272f80d48ff829a19ecf07807380300a98bc
Kevin Velickovic <kevin.velickovic@gmail.com>
2015-05-19 10:16:41 +0200

    Updated module version info

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 
 ## Changelog
 
+### 1.0.3 (May 19, 2015)
+
+- `UPDATED`: Module version info
+
 ### 1.0.2 (April 7, 2015)
 
 - `FIXED`: Fixed messages flushing (memory leak)
--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ from distutils.core import setup, Extension
 
 setup(
         name="pycheckjpeg",
-        version="0.1",
+        version="1.0.3",
         ext_modules = [
             Extension(
                 "pycheckjpeg",
